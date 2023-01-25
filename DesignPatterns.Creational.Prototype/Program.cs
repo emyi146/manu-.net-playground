67d0a4dbfc9a423f83c55565212cebee88b9b67e
manuel.garcia <manuel.garcia@frontiersin.org>
2023-01-25 23:39:03 +0100

    Add Prototype and Singleton patterns

---
+++ DesignPatterns.Creational.Prototype/Program.cs
@@ -0,0 +1,54 @@
+﻿// Prototype Design Pattern (C#)
+// https://www.youtube.com/watch?v=fqaoCDyxb1w&list=PLOeFnOV9YBa4ary9fvCULLn7ohNKR6Ees
+// Instances that implement a Copy method
+
+using Xunit;
+
+var seed1 = new TreeSeed("Apple");
+var seed2 = new FlowerSeed("Roses");
+var garden = new GardenFactory(seed1, seed2);
+
+var copySeed1 = garden.CreatePlant1();
+var copySeed2 = garden.CreatePlant2();
+
+Assert.NotEqual(seed1, copySeed1);
+Assert.NotEqual(seed2, copySeed2);
+
+class GardenFactory
+{
+    Seed _seed1;
+    Seed _seed2;
+
+    public GardenFactory(TreeSeed seed1, FlowerSeed seed2)
+    {
+        _seed1 = seed1;
+        _seed2 = seed2;
+    }
+
+    internal Seed CreatePlant1() => _seed1.Copy();
+    internal Seed CreatePlant2() => _seed2.Copy();
+}
+
+internal abstract class Seed
+{
+    internal abstract Seed Copy();
+}
+
+internal class TreeSeed : Seed
+{
+    internal string Type { get; }
+
+    internal TreeSeed(string type) => Type = type;
+
+    internal override Seed Copy() => new TreeSeed(Type);
+}
+
+
+internal class FlowerSeed : Seed
+{
+    internal string Type { get; }
+
+    internal FlowerSeed(string type) => Type = type;
+
+    internal override Seed Copy() => new FlowerSeed(Type);
+}
---
+++ DesignPatterns.Creational.Singleton/Program.cs
@@ -0,0 +1,114 @@
+﻿// Singleton Design Pattern (C#, Microservices)
+// https://www.youtube.com/watch?v=9_9hI69fwhg&list=PLOeFnOV9YBa4ary9fvCULLn7ohNKR6Ees&index=4
+// Use Approach 3!!!!
+
+using DesignPatterns.Common;
+
+"Approach 1".Dump();
+RunInMultipleThreads(() => MemoryCacheApproach1.Instance);
+/*
+ Result:
+    Created MemoryCacheApproach1 0
+    Created MemoryCacheApproach1 0
+    Created MemoryCacheApproach1 4
+    Created MemoryCacheApproach1 1
+    Created MemoryCacheApproach1 3
+    Created MemoryCacheApproach1 2
+    Created MemoryCacheApproach1 6
+    Created MemoryCacheApproach1 5
+ */
+"Approach 2".Dump();
+RunInMultipleThreads(() => MemoryCacheApproach2.Instance);
+/*
+ Result:
+    Created MemoryCacheApproach2 0
+ */
+"Approach 3".Dump();
+RunInMultipleThreads(() => MemoryCacheApproach3.Instance);
+/*
+ Result:
+    Created MemoryCacheApproach3 0
+ */
+Console.WriteLine();
+
+static void RunInMultipleThreads(Func<IMemoryCacheApproach> memoryCacheApproach)
+{
+    int threads = 8;
+    Task[] tasks = new Task[threads];
+    for (int i = 0; i < threads; i++)
+    {
+        tasks[i] = Task.Run(memoryCacheApproach);
+    }
+    Task.WaitAll(tasks);
+}
+
+public interface IMemoryCacheApproach
+{
+    public static IMemoryCacheApproach Instance { get; }
+}
+
+// Approach 1 - Not ideal, not thread-safe. It does not support multi-threading
+
+public class MemoryCacheApproach1 : IMemoryCacheApproach
+{
+    private static MemoryCacheApproach1? _instance;
+    private static int _i = 0;
+
+    private MemoryCacheApproach1()
+    {
+        $"Created MemoryCacheApproach1 {_i++}".Dump();
+    }
+
+    public static IMemoryCacheApproach Instance => _instance ??= new MemoryCacheApproach1();
+}
+
+
+// Approach 2 - Thread-safe! However, the static constructors will consume a lot of resources during startup, 
+// and it's also hard to manage dependencies with static ctor
+public class MemoryCacheApproach2 : IMemoryCacheApproach
+{
+    private static readonly MemoryCacheApproach2 _instance;
+    private static int _i = 0;
+
+    static MemoryCacheApproach2()
+    {
+        _instance = new MemoryCacheApproach2();
+    }
+
+    private MemoryCacheApproach2()
+    {
+        $"Created MemoryCacheApproach2 {_i++}".Dump();
+    }
+
+    public static IMemoryCacheApproach Instance => _instance;
+}
+
+
+// Approach 3 - Thread-safe, avoiding static constructors
+public class MemoryCacheApproach3 : IMemoryCacheApproach
+{
+    private static MemoryCacheApproach3 _instance;
+    private static int _i = 0;
+    private static object _lock = new object();
+
+    private MemoryCacheApproach3()
+    {
+        $"Created MemoryCacheApproach3 {_i++}".Dump();
+    }
+
+    public static IMemoryCacheApproach Instance
+    {
+        get
+        {
+            if (_instance is not null)
+            {
+                return _instance;
+            }
+
+            lock (_lock)
+            {
+                return _instance ??= new MemoryCacheApproach3();
+            }
+        }
+    }
+}
